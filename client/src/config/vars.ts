5e7d0339d55146659cbacff3e1f2b8c48e5af2a0
oscarheimdahl <o.heimdahl@gmail.com>
2022-09-07 13:51:37 +0200

    Prod env variable

--- client/src/config/vars.ts
+++ client/src/config/vars.ts
@@ -1,1 +1,4 @@
-export const host = 'http://localhost:3000/';
+// @ts-ignore
+export const host = import.meta.env.PROD ? '' : import.meta.env.VITE_DEV_SERVER_DOMAIN;
+
+console.log('host:', host);
--- server/app.ts
+++ server/app.ts
@@ -6,10 +6,9 @@ import roomRoutes from './routes/chatroom';
 import io from './routes/socket';
 import userRoutes from './routes/user';
 
-// dotenv.config();
 const app = express();
 const origin = '*';
-// // app.use(cors({ origin: 'http://localhost:8080' }));
+
 app.use(cors({ origin }));
 app.use(express.json());
 
@@ -17,6 +16,10 @@ app.use('/users', userRoutes);
 app.use('/rooms', roomRoutes);
 
 app.use('/', express.static(path.resolve(__dirname, '..', 'client', 'dist')));
+
+// app.get('*', (req, res) => {
+//   res.status(404).send('what???');
+// });
 
 const server = app.listen(3000, () => {
   console.log('http://localhost:3000');

0463010f193e129d4f837c318f93a7074dc44a4c
oscarheimdahl <o.heimdahl@gmail.com>
2022-09-08 16:42:17 +0200

    test8

--- deploy.sh
+++ deploy.sh
@@ -25,9 +25,9 @@ git fetch
  then
    echo -e ${ERROR}Not up to date with origin.${NOCOLOR}
    echo
-   lsof -t -i tcp:3000 | xargs kill
-   git pull
-   yarn deploy
+   /usr/bin/killall screen
+   /usr/bin/git pull
+   /usr/bin/screen -d -m /usr/bin/bash -c "sleep 4; cd ~/Desktop/chatup && /usr/bin/git pull && /usr/local/bin/yarn deploy; sleep 4"
    /usr/bin/echo "running" > /home/oheim/Desktop/run.txt
    exit 0
  else
